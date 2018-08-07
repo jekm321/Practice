40e3cdfa90ea61402a62e1efda65c70f82ee810e
Justin M <jekm321@gmail.com>
2018-08-07 19:52:07 -0400

    testing out match on how to format time

---
+++ calculating datetime.js
@@ -0,0 +1,18 @@
+let timeelapsed = 300000;
+
+let date = new Date(1533689000000)
+let date2 = new Date(1533689390000)
+
+let timeElapsed = 1533689390000 - 1533689000000;
+
+console.log(date);
+console.log(date2);
+console.log(timeElapsed);
+let timeLeft = (600000 - timeElapsed)/1000;
+
+console.log(timeLeft)
+
+console.log(600 - 590)
+
+console.log(parseInt(10 / 60))
+console.log(210 % 60)
